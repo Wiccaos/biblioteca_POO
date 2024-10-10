be45a938099b721220e702ea5d74a2819f3e9339
Wiccaos <wiccaos.moon@outlook.com>
2024-10-10 01:10:00 -0300

    editado

--- clases/Biblioteca.py
+++ clases/Biblioteca.py
@@ -1,18 +1,8 @@
 class biblioteca():
-    def __init__(self, nombre, direccion, r_s, run_bblio, telefono):
+    """La Clase Biblioteca, que contiene los datos principales de la Biblioteca"""
+    def __init__(self, rut_biblioteca, nombre, direccion, telefono):
         self.nombre = nombre
+        self.rut_biblioteca = rut_biblioteca
         self.direccion = direccion
-        self.r_s = r_s
-        self.run_bblio = run_bblio
         self.telefono = telefono
 
-    def buscar_libro():
-        pass
-
-    def devolver_libro():
-        pass
-
-    def prestar_libro():
-        pass
-
-    
---
+++ clases/Bibliotecario.py
@@ -0,0 +1,30 @@
+import Persona
+
+class Bibliotecario(Persona):
+    def __init__(self, rut_bibliotecario, id_bibliotecario, nombre_bibl, apellido_bibl):
+        super().__init__.Persona(rut) = rut_bibliotecario
+        self.id_bibliotecario = id_bibliotecario
+        super().__init__.Persona(nombre) = nombre_bibl
+        super().__init__.Persona(apellido) = apellido_bibl
+
+    #Metodos
+    def agregar_libro():
+        # código para agregar un libro a la base de datos
+        pass
+
+    def eliminar_libro():
+        # código para eliminar un libro de la base de datos
+        pass
+
+    def registrar_prestamo():
+        # código para registrar un préstamo de un libro
+        pass
+
+    def registrar_devolucion():
+        # código para registrar la devolución de un libro
+        pass
+
+    def registrar_usuario():
+        # código para registrar un usuario
+        pass
+    
