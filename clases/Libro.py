5b0c6107a505f8618190774db1646ad574a986fc
Wiccaos <wiccaos.moon@outlook.com>
2024-10-09 22:06:04 -0300

    editado el archivo libro

--- clases/Libro.py
+++ clases/Libro.py
@@ -4,7 +4,7 @@ import editorial
 class Libro(editorial):
     def __init__(self, titulo_libro, id_autor, isbn_libro, run_editorial):
         #el atributo run_editorial es herencia de la clase libro y editorial
-        super.__init__.editorial = run_editorial
+        super().__init__.editorial(run_editorial) = run_editorial
         self.isbn_libro = isbn_libro
         self.titulo_libro = titulo_libro
         self.id_autor = id_autor
